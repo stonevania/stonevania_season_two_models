8986ed4a4cc738c8779e4ab30827b78aaa9ce175
dadmannwalking <dadmannwalkingyt@gmail.com>
2025-09-19 09:36:41 -0400

    Update build.sh

--- releases/final/build.sh
+++ releases/final/build.sh
@@ -1,1 +1,11 @@
+### Push generated release to new repo ###
+echo "Pushing new release to release repo..."
 
+# Prepare git for push
+git add release.zip
+git add hash.txt
+git add README.md
+
+# Push new release
+git commit -m "new release"
+git push -u origin main
