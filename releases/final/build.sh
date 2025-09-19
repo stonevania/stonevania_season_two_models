735fb86640d33c91546e8bc3bb0c968d1c502ca2
dadmannwalking <dadmannwalkingyt@gmail.com>
2025-09-19 09:38:48 -0400

    Update build.sh

--- releases/final/build.sh
+++ releases/final/build.sh
@@ -6,9 +6,9 @@ git config --global user.name "dadmannwalking"
 git config --global user.email "dadmannwalkingyt@gmail.com"
 
 # Prepare git for push
-git add release.zip
-git add hash.txt
-git add README.md
+git add ./releases/final/release.zip
+git add ./releases/final/hash.txt
+git add ./releases/final/README.md
 
 # Push new release
 git commit -m "new release"
