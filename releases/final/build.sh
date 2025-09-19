1a2bc019151245fbab7c4c9de4a92c35c31db967
dadmannwalking <dadmannwalkingyt@gmail.com>
2025-09-19 09:40:06 -0400

    Update build.sh

--- releases/final/build.sh
+++ releases/final/build.sh
@@ -5,11 +5,7 @@ echo "Pushing new release to release repo..."
 git config --global user.name "dadmannwalking"
 git config --global user.email "dadmannwalkingyt@gmail.com"
 
-# Prepare git for push
-git add ./releases/final/release.zip
-git add ./releases/final/hash.txt
-git add ./releases/final/README.md
-
-# Push new release
+# Prepare, commit, and push new release
+git add .
 git commit -m "new release"
 git push -u origin main
